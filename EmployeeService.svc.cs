1203fa19449215a1ec25be9be25d770e3d33d6fd
Vigneshvaran N <vighu1610@gmail.com>
2023-04-27 09:04:51 +0530

    Add project files.

---
+++ EmployeeService.svc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfService.Models;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "EmployeeService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select EmployeeService.svc or EmployeeService.svc.cs at the Solution Explorer and start debugging.
+    public class EmployeeService : IEmployeeService
+    {
+         employeesEntities1 entity = new employeesEntities1();
+        public void AddEmployee(employee emp)
+        {
+            entity.employees.Add(emp);
+            entity.SaveChanges();
+        }
+        public employee GetEmployees(int id)
+        {
+            List<employee> eList = new List<employee>();
+            employee emp1 =  entity.employees.Find(id);
+
+            return emp1;
+        }
+        public void updateEmployee(int empid,string updatedName)
+        {
+            employee values = entity.employees.FirstOrDefault(emp => emp.empno == empid);
+            if(values!=null)
+            {
+                values.ename = updatedName;
+            }
+            entity.SaveChanges();                        
+        }
+        public void deleteEmployee(int empid)
+        {
+            employee value = entity.employees.FirstOrDefault(emp => emp.empno == empid);
+            entity.employees.Remove(value);
+            entity.SaveChanges();
+        }
+    }
+}
---
+++ IEmployeeService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfService.Models;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IEmployeeService" in both code and config file together.
+    [ServiceContract]
+    public interface IEmployeeService
+    {
+        [OperationContract]
+        void AddEmployee(employee emp);
+        [OperationContract]
+        employee GetEmployees(int empno);
+
+        [OperationContract]
+        void updateEmployee(int id,string updatedName);
+        
+        [OperationContract]
+        void deleteEmployee(int id);
+
+    }
+
+}
